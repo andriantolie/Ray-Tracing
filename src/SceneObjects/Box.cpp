79f5aaa81deb56cfdcd282bae81e2b613393ba71
shiftahead <shiftahead@gmail.com>
2014-11-05 23:21:03 +0800

    Some more ray files plus the box intersection

--- src/SceneObjects/Box.cpp
+++ src/SceneObjects/Box.cpp
@@ -12,38 +12,46 @@ bool Box::intersectLocal( const ray& r, isect& i ) const
 	vec3f d = r.getDirection();
 
 
-	double tMin = -1.0e308;
-	double tMax = 1.0308;
+	double tMin = -1.0e100;
+	double tMax = 1.0e100;
 
 	for (int currentaxis = 0; currentaxis < 3; currentaxis++) {
 
 		double vd = d[currentaxis];
 		double vp = p[currentaxis];
 
-		if (vd == 0) {
+		if (vd == 0.0) {
 			if (vp < min[currentaxis] || vp > max[currentaxis])
-				continue;
+				return false;
 		}
+		else {
+			double v1 = min[currentaxis] - p[currentaxis];
+			double v2 = max[currentaxis] - p[currentaxis];
+			double t1 = v1 / vd;
+			double t2 = v2 / vd;
 
-		double v1 = min[currentaxis] - p[currentaxis];
-		double v2 = max[currentaxis] - p[currentaxis];
-		double t1 = v1 / vd;
-		double t2 = v2 / vd;
+			if (t1 > t2) { // swap t1 & t2
+				double ttemp = t1;
+				t1 = t2;
+				t2 = ttemp;
+			}
+			if (t1 > tMin)
+				tMin = t1;
+			if (t2 < tMax)
+				tMax = t2;
 
-		if (t1 > t2) { // swap t1 & t2
-			double ttemp = t1;
-			t1 = t2;
-			t2 = ttemp;
+			if (tMin > tMax) // box is missed
+				return false;
+			if (tMax < 0.0) // box is behind ray
+				return false;
+			if (tMin <= RAY_EPSILON)
+				return false;
 		}
-		if (t1 > tMin)
-			tMin = t1;
-		if (t2 < tMax)
-			tMax = t2;
-
-		if (tMin > tMax) // box is missed
-			return false;
-		if (tMax < 0.0) // box is behind ray
-			return false;
 	}
+
+	i.obj = this;
+	i.t = tMin;
+	i.N = r.at(tMin).normalize();
+
 	return true;
 }
